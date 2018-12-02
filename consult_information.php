58e7e9e705e263dc09d1c86b122e33e7da1beb36
BuildTools <unconfigured@null.spigotmc.org>
2018-12-02 17:25:11 +0000

    pagina 6 feita (acho eu)

--- consult_information.php
+++ consult_information.php
@@ -26,7 +26,7 @@
 
 
             //Issuing MySQL command
-            $sql = " SELECT * FROM animal INNER JOIN consult NATURAL JOIN consult_diagnosis NATURAL JOIN procedure_ WHERE animal.name = '$animal_name' AND animal.VAT =".intval($owner_VAT)."AND consult.date_timestamp = '$consult_date';";
+            $sql = " SELECT * FROM animal INNER JOIN consult NATURAL JOIN consult_diagnosis NATURAL JOIN procedure_ WHERE animal.name = '$animal_name' AND animal.VAT =".intval($owner_VAT)." AND consult.date_timestamp = CAST('$consult_date' as datetime)";
             $result = $connection->query($sql);
 
             if ($result == FALSE)
@@ -71,6 +71,7 @@
 
                   $species = $row['species_name'];
                   $colour = $row['colour'];
+                  $gender = $row['gender'];
                   $birth_year = $row['birth_year'];
                   $code = $row['code'];
                   $num = $row['num'];
@@ -90,6 +91,7 @@
 
                   echo "<td>" . $species . "</td>";
                   echo "<td>" . $colour . "</td>";
+                  echo "<td>" . $gender . "</td>";
                   echo "<td>" . $birth_year . "</td>";
                   echo "<td>" . $code . "</td>";
                   echo "<td>" . $num . "</td>";
@@ -101,6 +103,10 @@
 
             $connection = null;
             echo("<form action='blood_test_results.php' method='post'>
+              <input type="hidden" name="animal_name" value=$animal_name>
+              <input type="hidden" name="owner_VAT" value=$owner_VAT>
+              <input type="hidden" name="date_time" value=$consult_date>
+              <input type="hidden" name="procedure_num" value=$num>
               <p> <input type='submit' value='Insert Boold Test Results'> </p>
             </form>");
 
