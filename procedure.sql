239b2c64e55c1f126d3146f529dd3f2802b7aa45
BuildTools <unconfigured@null.spigotmc.org>
2018-12-05 12:05:45 +0000

    Comentei cenas no procedure

--- procedure.sql
+++ procedure.sql
@@ -1,2 +1,2 @@
-PROCEDURE:
-delimiter $$ drop procedure if exists ref_values$$ CREATE procedure ref_values() begin UPDATE indicator, produced_indicator SET indicator.units='cg', produced_indicator.value=produced_indicator.value *0.1, indicator.reference_value=indicator.reference_value *0.1 WHERE produced_indicator.indicator_name=indicator.name AND indicator.units='mg'; end$$ delimiter ; 
+--PROCEDURE:
+delimiter $$ drop procedure if exists ref_values$$ CREATE procedure ref_values() begin UPDATE indicator, produced_indicator SET indicator.units='cg', produced_indicator.value=produced_indicator.value *0.1, indicator.reference_value=indicator.reference_value *0.1 WHERE produced_indicator.indicator_name=indicator.name AND indicator.units='mg'; end$$ delimiter ;
